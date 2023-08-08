fa523b7b0152d408974696665e9a143154e0621d
ChoudharyKunal <choudharykunal8266@gmail.com>
2023-08-09 02:36:38 +0530

    TRIED

--- Python/automation/auto2.py
+++ Python/automation/auto2.py
@@ -1,8 +1,12 @@
+import time
+
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 import pandas as pd
 import os
 import sys
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 
 path = './chromedriver.exe'
@@ -11,6 +15,7 @@ driver = webdriver.Chrome(service=service)
 
 application_path = os.path.dirname(sys.executable)
 website = "https://www.youtube.com/"
+NewTablink = Keys.chord(Keys.CONTROL, Keys.ENTER);
 
 driver.get(website)
 
@@ -22,19 +27,24 @@ search_btn = driver.find_element(by="xpath", value='//button[@id="search-icon-le
 
 search_btn.click()
 driver.implicitly_wait(10)
+driver.execute_script("window.scrollTo(0,4000)")
 
 ytVideoContainer = driver.find_elements(by="xpath", value='//body/ytd-app[1]/div[1]/ytd-page-manager[1]/ytd-search[1]/div[1]/ytd-two-column-search-results-renderer[1]/div[1]/ytd-section-list-renderer[1]/div[2]/ytd-item-section-renderer[1]/div[3]/ytd-video-renderer')
+
 videoTitle = []
 videoViews = []
 videoLink = []
 
 for videos in ytVideoContainer:
-    titlaValue = videos.find_element(by="xpath", value=".//div[1]/div[1]/div[1]/div[1]/h3[1]").text
-    vLink = videos.find_element(by="xpath",value='.//div[1]/div[1]/div[1]/div[1]/h3[1]/a[1]').get_attribute("href")
-    vViews = videos.find_element(by="xpath", value='.//div[1]/ytd-video-meta-block[1]/div[1]/div[2]/span[1]').text
-    videoTitle.append(titlaValue)
-    videoLink.append(vLink)
-    videoViews.append(vViews)
+        titlaValue = videos.find_element(by="xpath", value=".//div[1]/div[1]/div[1]/div[1]/h3[1]").text
+        vLink = videos.find_element(by="xpath",value='.//div[1]/div[1]/div[1]/div[1]/h3[1]/a[1]').get_attribute("href")
+        vViews = videos.find_element(by="xpath", value='.//div[1]/ytd-video-meta-block[1]/div[1]/div[2]/span[1]').text
+        videoTitle.append(titlaValue)
+        videoLink.append(vLink)
+        videoViews.append(vViews)
+        driver.NewTabLink(vLink)
+        #ActionChains().key_down(Keys.CONTROL).click(vLink).key_up(Keys.CONTROL).perform()
+        time.sleep(3)
 
 ytDict = {'title':videoTitle, 'Views': videoViews, "Video Link": videoLink}
 
