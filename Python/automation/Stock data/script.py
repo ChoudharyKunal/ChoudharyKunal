ecead5a40c164fb5438f9ae74c1a59e1ffdfb01b
ChoudharyKunal <choudharykunal8266@gmail.com>
2023-08-27 02:15:31 +0530

    fetching stock details

--- Python/automation/Stock data/script.py
+++ Python/automation/Stock data/script.py
@@ -14,6 +14,7 @@ import selenium.webdriver.support.expected_conditions
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
 
 path = './chromedriver.exe'
 service = Service(executable_path=path)
@@ -23,11 +24,16 @@ application_path = os.path.dirname(sys.executable)
 website = "https://finance.yahoo.com/"
 
 StockList = ["TCS.NS","M&M.NS"]
+stockCurrentPrice = []
+stockLossAmt = []
+stockLossPercent = []
+
+
 def btnClick():
     try:
         searchBtn = driver.find_element(by="xpath",
                                         value="/html[1]/body[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[3]/div[2]/div[1]/div[1]/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/form[1]/button[2]")
-        searchBtn.click()
+        searchBtn.send_keys(Keys.ENTER)
     except Exception as e:
         print("got exeception error on btn click: "+e)
 
@@ -36,32 +42,42 @@ def searchStock(stockname):
     try:
         searchField = driver.find_element(by="xpath",
                                          value="/html[1]/body[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[3]/div[2]/div[1]/div[1]/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/form[1]/input[1]")
-
-   #     searchField1 = driver.find_element(by="xpath", value='//form[1]/input[1]')
         searchField.send_keys(stockname)
-
         btnClick()
+        time.sleep(10)
+        stockCurrentPriceValue = driver.find_element(by="xpath", value = '/html[1]/body[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[6]/div[1]/div[3]/div[1]/div[1]/fin-streamer[1]').text
+        stockLossAmtValue = driver.find_element(by="xpath", value = '/html[1]/body[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[6]/div[1]/div[3]/div[1]/div[1]/fin-streamer[2]').text
+        stockLossPercentvalue = driver.find_element(by="xpath", value = '/html[1]/body[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[6]/div[1]/div[3]/div[1]/div[1]/fin-streamer[3]').text
+
+        stockCurrentPrice.append(stockCurrentPriceValue)
+        stockLossAmt.append(stockLossAmtValue)
+        stockLossPercent.append(stockLossPercentvalue)
+
+
+
 
     except Exception as e:
         print("got exeception error: on searchStock "+e)
 
 
-driver.get(website)
+
 
 for stockName in StockList:
     try:
-        searchStock(stockName)
+        driver.get(website)
         time.sleep(5)
+        searchStock(stockName)
         print(stockName+": done")
         time.sleep(10)
     except Exception as e :
         print("got execetion at looping for stock values"+e)
 
-#search_btn.click()
 
-#seach_field = driver.find_element(by="xpath", value = '//input[@class = "input-KLRTYDjH"')
-
-#seach_field.send_keys("TCS")\
 
 driver.close()
 
+#/html[1]/body[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/a[1]
+
+# share price = /html[1]/body[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[6]/div[1]/div[3]/div[1]/div[1]/fin-streamer[1]
+#share change = /html[1]/body[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[6]/div[1]/div[3]/div[1]/div[1]/fin-streamer[2]
+# share percent change = /html[1]/body[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[6]/div[1]/div[3]/div[1]/div[1]/fin-streamer[3]
